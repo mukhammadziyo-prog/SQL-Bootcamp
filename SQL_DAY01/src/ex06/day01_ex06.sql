e50a42bb3d91e9db80207353f8128180332a2d38
Mukhammadziyo <244293343+mukhammadziyo-prog@users.noreply.github.com>
2026-08-25 00:38:42 +0500

    Fix SQL syntax by adding a semicolon

--- SQL_DAY01/src/ex06/day01_ex06.sql
+++ SQL_DAY01/src/ex06/day01_ex06.sql
@@ -5,4 +5,4 @@ INTERSECT
 SELECT visit_date AS action_date,
        (SELECT name FROM person WHERE id = person_visits.person_id) AS person_name
 FROM person_visits
-ORDER BY action_date, person_name DESC
+ORDER BY action_date, person_name DESC;
